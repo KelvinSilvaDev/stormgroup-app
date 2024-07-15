e8cb344461d3dee407759ebb6db8e4dce2ffa72d
kelvin.romao <kelvin.romao@gauge.haus>
2024-07-15 07:47:35 -0300

    lint

--- src/hooks/withAuth.tsx
+++ src/hooks/withAuth.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/display-name */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { getRole, isAuthenticated } from '@/service/auth'
