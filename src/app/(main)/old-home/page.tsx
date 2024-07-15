bd36425aa4d999673421b8f1a670fd3377278e3f
kelvin.romao <kelvin.romao@gauge.haus>
2024-07-15 07:51:18 -0300

    removendo arquivos não usados

--- src/app/(main)/old-home/page.tsx
+++
@@ -1,9 +0,0 @@
-import MovieSearch from './search/MovieSearch'
-
-export default function Home() {
-  return (
-    <main className="flex h-full flex-1 flex-col items-center justify-center p-16 align-middle">
-      <MovieSearch />
-    </main>
-  )
-}
